7c25fc9319cd6fb121d101ad4c79e4a36b7a5285
mikam-dev <kamilosmike@gmail.com>
2024-02-01 16:55:04 -0600

    initial setup

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,3 +3,15 @@ type NavLink = {
 	link: string,
 	order: number
 }
+
+type APODImage = {
+	title: string,
+	date: string,
+	url: string,
+	hdurl?: string,
+	media_type: "image" | "video",
+	explanation: string,
+	thumbnail_url?: string,
+	copyright?: string,
+	service_version: string,
+}
