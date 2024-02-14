310a76cb34aafe64aeb2f20f3ef47ac612ad5680
mikam-dev <kamilosmike@gmail.com>
2024-02-14 10:47:00 -0600

    getting image and description from apod api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,31 @@
 import Hero from "@/components/layout/Hero";
+import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const data: APODImage = await getImage();
+
   return (
-    <main className="flex bg-background min-h-[100svh] pt-[8svh] pb-8 flex-col items-center justify-between">
+    <main className="flex bg-background min-h-[100vh] pb-8 flex-col items-center justify-between">
       <Hero>
-        <h1 className="text-6xl font-bold">Clean.js</h1>
-        <p className="text-2xl">This is a Next.js + Tailwind CSS starter template.</p>
+        <div className="w-full md:w-[50%] p-4">
+          <Image src={data.url} alt={data.title} width={600} height={400} className="w-full max-w-2xl h-auto" />
+        </div>
+        <div className="w-full md:w-[50%] p-4">
+          <h1 className="text-4xl font-bold mt-4 text-center md:text-start">{data.title}</h1>
+          <p className="mt-4">{data.explanation}</p>
+        </div>
       </Hero>
     </main>
   );
+}
+
+const getImage = async () => {
+  const baseUrl = `https://api.nasa.gov/planetary/apod?api_key=${process.env.NEXT_PUBLIC_NASA_API_KEY}`;
+
+  try {
+    const res = await fetch(`${baseUrl}`);
+    return res.json();
+  } catch (error) {
+    console.error(error);
+  }
 }
