26db7b898d532d8397fda32757dfc4e80af11c3d
mikam-dev <kamilosmike@gmail.com>
2024-02-15 10:52:59 -0600

    changes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -25,7 +25,22 @@ export const getImage = async (date?: string) => {
   }
 }
 
-export const getImages = async (startDate?: string, endDate?: string) => {
+export const getRandomImages = async () => {
+  const baseUrl = `https://api.nasa.gov/planetary/apod?api_key=${process.env.NEXT_PUBLIC_NASA_API_KEY}`;
+
+  try {
+    const images = await fetch(`${baseUrl}&count=12`, {
+      method: 'GET',
+      next: { revalidate: 600 }
+    }).then((images) => images.json());
+
+    return images;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export const getImagesInRange = async (startDate?: string, endDate?: string) => {
   const baseUrl = `https://api.nasa.gov/planetary/apod?api_key=${process.env.NEXT_PUBLIC_NASA_API_KEY}`;
 
   if (!startDate || !endDate) {
@@ -42,7 +57,7 @@ export const getImages = async (startDate?: string, endDate?: string) => {
   }
 
   try {
-    const images = await fetch(`${baseUrl}&count=12`, {
+    const images = await fetch(`${baseUrl}&start_date=${startDate}&end_date=${endDate}`, {
       method: 'GET',
       next: { revalidate: 600 }
     }).then((images) => images.json());
