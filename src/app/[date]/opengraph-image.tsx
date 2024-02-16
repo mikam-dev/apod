608d357f6a994f8180a7e26e153b2a5b7d0132c7
mikam-dev <kamilosmike@gmail.com>
2024-02-16 10:08:38 -0600

    minor styling changes

---
+++ src/app/[date]/opengraph-image.tsx
@@ -0,0 +1,46 @@
+import { getImage } from "@/lib/utils";
+import { Inter } from "next/font/google";
+import { ImageResponse } from 'next/og';
+
+export const runtime = 'edge'
+
+export const alt = 'Astronomy Picture of the Day'
+export const size = {
+	width: 1200,
+	height: 630,
+}
+export const contentType = 'image/png'
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default async function Image({ params }: { params: { date: string } }) {
+	const { date } = params;
+	const image: APODImage = await getImage(date);
+
+	return new ImageResponse(
+		(
+			<div
+				style={{
+					fontFamily: inter + ', sans-serif',
+					fontSize: 48,
+					background: 'white',
+					width: '100%',
+					height: '100%',
+					display: 'flex',
+					alignItems: 'center',
+					justifyContent: 'center',
+				}}
+			>
+				{image.media_type === 'image' && (
+					<img src={image.url} alt={image.title} />
+				)}
+				{image.media_type === 'video' && (
+					<img src={image.thumbnail_url} alt={image.title} />
+				)}
+			</div>
+		),
+		{
+			...size,
+		}
+	)
+}
---
+++ src/app/[date]/twitter-image.tsx
@@ -0,0 +1,43 @@
+import { getImage } from "@/lib/utils";
+import { ImageResponse } from 'next/og';
+
+export const runtime = 'edge'
+
+export const alt = 'About Acme'
+export const size = {
+	width: 1200,
+	height: 630,
+}
+export const contentType = 'image/png'
+
+export default async function Image({ params }: { params: { date: string } }) {
+	const { date } = params;
+	const image: APODImage = await getImage(date);
+
+	return new ImageResponse(
+		(
+			<div
+				style={{
+					fontSize: 48,
+					background: 'white',
+					width: '100%',
+					height: '100%',
+					display: 'flex',
+					alignItems: 'center',
+					justifyContent: 'center',
+				}}
+			>
+				{image.media_type === 'image' && (
+					<img src={image.url} alt={image.title} />
+				)}
+				{image.media_type === 'video' && (
+					<img src={image.thumbnail_url} alt={image.title} />
+				)}
+				<p>{image.title}</p>
+			</div>
+		),
+		{
+			...size,
+		}
+	)
+}
