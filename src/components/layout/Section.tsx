8dab907cd0b5d74be42d8dad8ac278ede5163ae6
Mike Kamilos <64214237+mikam-dev@users.noreply.github.com>
2024-02-18 14:20:13 -0600

    Update Section.tsx

--- src/components/layout/Section.tsx
+++ src/components/layout/Section.tsx
@@ -15,7 +15,7 @@ export function Section({
 }) {
 
 	return (
-		<section className={`flex items-center justify-center w-full min-h-[92vh] bg-gradient-to-b from-background to-muted m-0 p-4 sm:p-6 md:p-8 lg:p-12 xl:p-16`}>
+		<section className={`flex items-center justify-center w-full min-h-[92vh] bg-gradient-to-b from-background to-muted m-0 md:p-8 lg:p-12 xl:p-16`}>
 			<div className={cn(`flex ${rx ? 'md:flex-row-reverse' : 'md:flex-row'} ${ry ? 'flex-col-reverse' : 'flex-col'} items-start justify-between w-full max-w-6xl m-0 space-y-2 md:space-x-4 md:space-y-0`, className)}>
 				{children}
 			</div>
