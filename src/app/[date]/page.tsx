3e08f3109c5246a5508039c8941a4e8ee74c5581
mikam-dev <kamilosmike@gmail.com>
2024-02-18 17:48:05 -0600

    fixing styling on apod component

--- src/app/[date]/page.tsx
+++ src/app/[date]/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@/components/ui/button';
 import { format, parseISO } from 'date-fns';
+import { ExternalLink } from 'lucide-react';
 import { Metadata } from 'next';
 // import Image from 'next/image';
 import Link from 'next/link';
@@ -29,7 +30,7 @@ export default async function Page({ params }: { params: { date: string } }) {
 			<div className="w-full max-w-5xl h-auto flex flex-col bg-muted pb-4 items-center justify-start">
 				<h1 className="w-full p-2 pb-4 bg-primary text-primary-foreground text-4xl font-bold text-center">{image.title}</h1>
 				<h2 className="w-full p-2 bg-accent text-accent-foreground text-lg font-semibold text-center">{format(parseISO(image.date), 'MMMM dd, yyyy')}</h2>
-				<div className="w-full flex flex-col items-center bg-popover text-popover-foreground md:p-4 lg:rounded-b-xl">
+				<div className="w-full flex flex-col items-center bg-popover text-popover-foreground lg:p-4 lg:rounded-b-xl">
 					{image.media_type === 'video' && (
 						<iframe
 							width={560}
@@ -38,7 +39,7 @@ export default async function Page({ params }: { params: { date: string } }) {
 							title="YouTube video player"
 							allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
 							allowFullScreen
-							className={`w-full max-w-4xl h-auto rounded-xl`}></iframe>
+							className={`w-full max-w-4xl h-auto lg:rounded-xl`}></iframe>
 					)}
 					{image.media_type === 'image' && (
 						/* eslint-disable-next-line */
@@ -47,26 +48,29 @@ export default async function Page({ params }: { params: { date: string } }) {
 							alt={image.title}
 							width={800}
 							height={450}
-							className={`w-full max-w-4xl h-auto rounded-2xl`}
+							className={`w-full max-w-4xl h-auto lg:rounded-2xl`}
 						/>
 						// <Image
 						// 	src={image.url}
 						// 	alt={image.title}
 						// 	width={800}
 						// 	height={450}
-						// 	className={`w-full max-w-4xl h-auto rounded-3xl`}
+						// 	className={`w-full max-w-4xl h-auto lg:rounded-2xl`}
 						// />
 					)}
 					{image.copyright && (
-						<caption className={`w-full font-extralight text-sm mt-4 px-2`}>
+						<caption className={`w-full font-extralight text-sm my-4 px-2 lg:mb-0`}>
 							&copy; {image.copyright}
 						</caption>
 					)}
 				</div>
-				<div className="w-full p-4 flex flex-col justify-start items-center text-lg text-foreground">
+				<div className="w-full p-4 flex flex-col justify-start items-center text-lg text-foreground space-y-2">
 					{(image.media_type === 'image') && (
 						<Link href={image.hdurl || image.url}>
-							<Button variant="ghost" className="text-sm">View Image</Button>
+							<Button variant="outline" className="text-sm pr-2 mb-2">
+								View Image
+								<sup><ExternalLink className="ml-1 p-0 w-3 h-3" /></sup>
+							</Button>
 						</Link>
 					)}
 					<p className="w-full px-2 max-w-4xl">{image.explanation}</p>
